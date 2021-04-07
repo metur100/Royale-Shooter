33f94d25e42a8bb60a8c02aef6db46ff105c20d3
metur100 <medin_93@live.com>
2021-04-07 18:51:08 +0200

    Add player jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,14 +7,15 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed;
     private Rigidbody rb;
     public float sprintModifier;
-    //public float jumpForce;
-    //public Transform groundDetector;
-    //public LayerMask ground;
+
+    public float jumpForce;
+    public Transform groundDetector;
+    public LayerMask ground;
+
     public Camera normalCam;
     private float baseFOV;
     private float sprintFOVModifier = 1.5f;
 
-    // Start is called before the first frame update
     void Start()
     {
         baseFOV = normalCam.fieldOfView;
@@ -23,23 +23,22 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         float horizontalMove = Input.GetAxisRaw("Horizontal");
         float verticalMove = Input.GetAxisRaw("Vertical");
 
         bool sprint = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        //bool jump = Input.GetKeyDown(KeyCode.Space);
+        bool jump = Input.GetKey(KeyCode.Space);
 
-        //bool isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, 0.1f, ground);
-        //bool isJumping = jump && isGrounded;
-        bool isSprinting = sprint && verticalMove > 0/* && !isJumping && isGrounded*/;
+        bool isGrounded = Physics.Raycast(groundDetector.position, Vector3.down, 0.1f, ground);
+        bool isJumping = jump && isGrounded;
+        bool isSprinting = sprint && verticalMove > 0 && !isJumping && isGrounded;
 
-        //if (isJumping)
-        //{
-        //    rb.AddForce(Vector3.up * jumpForce);
-        //}
+        if (isJumping)
+        {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
 
         Vector3 direction = new Vector3(horizontalMove, 0, verticalMove);
         direction.Normalize();
@@ -50,7 +49,9 @@ public class PlayerMovement : MonoBehaviour
             adjustSpeed *= sprintModifier;
         }
 
-        rb.velocity = transform.TransformDirection(direction) * adjustSpeed * Time.deltaTime;
+        Vector3 targetVelocity = transform.TransformDirection(direction) * adjustSpeed * Time.deltaTime;
+        targetVelocity.y = rb.velocity.y;
+        rb.velocity = targetVelocity;
 
         if (isSprinting)
         {
@@ -61,7 +62,5 @@ public class PlayerMovement : MonoBehaviour
             normalCam.fieldOfView = Mathf.Lerp(normalCam.fieldOfView, baseFOV, Time.deltaTime * 8f);
         }
 
-        //t_targetVelocity.y = rb.velocity.y;
-        //rb.velocity = t_targetVelocity;
     }
 }
